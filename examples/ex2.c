945137965764452a0fb4a99662ac09293100c91e
Rob Loach <robloach@gmail.com>
2024-04-15 14:13:40 -0400

    Update

---
+++ examples/ex2.c
@@ -0,0 +1,90 @@
+#define PNTR_APP_IMPLEMENTATION
+#include "pntr_app.h"
+
+#define PNTR_PORTABLEGL_IMPLEMENTATION
+#include "../pntr_portablegl.h"
+
+typedef struct AppData {
+    glContext* the_Context;
+} AppData;
+
+void smooth_vs(float* vs_output, pgl_vec4* vertex_attribs, Shader_Builtins* builtins, void* uniforms);
+void smooth_fs(float* fs_input, Shader_Builtins* builtins, void* uniforms);
+
+bool Init(pntr_app* app) {
+    AppData* appData = pntr_load_memory(sizeof(AppData));
+    pntr_app_set_userdata(app, appData);
+
+    // PortableGL setup
+    appData->the_Context = pntr_init_glContext(app->screen);
+	set_glContext(appData->the_Context);
+
+    // Set up the OpenGL state
+	GLenum smooth[4] = { PGL_SMOOTH4 };
+
+	float points_n_colors[] = {
+		-0.5, -0.5, 0.0,
+		 1.0,  0.0, 0.0,
+
+		 0.5, -0.5, 0.0,
+		 0.0,  1.0, 0.0,
+
+		 0.0,  0.5, 0.0,
+		 0.0,  0.0, 1.0
+    };
+
+	GLuint triangle;
+	glGenBuffers(1, &triangle);
+	glBindBuffer(GL_ARRAY_BUFFER, triangle);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points_n_colors), points_n_colors, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*6, 0);
+	glEnableVertexAttribArray(4);
+	glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(float)*6, (void*)(sizeof(float)*3));
+
+	// Note, no uniforms used in these shaders so no need to set any
+	GLuint myshader = pglCreateProgram(smooth_vs, smooth_fs, 4, smooth, GL_FALSE);
+	glUseProgram(myshader);
+
+    return true;
+}
+
+bool Update(pntr_app* app, pntr_image* screen) {
+    AppData* appData = (AppData*)pntr_app_userdata(app);
+
+    // Draw the triangle
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    return true;
+}
+
+void Close(pntr_app* app) {
+    AppData* appData = (AppData*)pntr_app_userdata(app);
+
+	// PortableGL cleanup
+	free_glContext(appData->the_Context);
+
+    pntr_unload_memory(appData);
+}
+
+void smooth_vs(float* vs_output, pgl_vec4* vertex_attribs, Shader_Builtins* builtins, void* uniforms) {
+	((pgl_vec4*)vs_output)[0] = vertex_attribs[4]; //color
+	builtins->gl_Position = vertex_attribs[0];
+}
+
+void smooth_fs(float* fs_input, Shader_Builtins* builtins, void* uniforms) {
+	builtins->gl_FragColor = ((pgl_vec4*)fs_input)[0];
+}
+
+pntr_app Main(int argc, char* argv[]) {
+    return (pntr_app) {
+        .width = 640,
+        .height = 480,
+        .title = "pntr_portablegl",
+        .init = Init,
+        .update = Update,
+        .close = Close,
+        .fps = 60
+    };
+}
--- pntr_portablegl.h
+++ pntr_portablegl.h
@@ -3,22 +3,37 @@
 
 #ifndef PGL_MALLOC
 #define PGL_MALLOC(sz)      PNTR_MALLOC(sz)
-#endif
+#endif  // PGL_MALLOC
+
 #ifndef PGL_REALLOC
 #define PGL_REALLOC(p, sz)  PNTR_REALLOC(p, sz)
-#endif
+#endif  // PGL_REALLOC
+
 #ifndef PGL_FREE
 #define PGL_FREE(p)         PNTR_FREE(p)
-#endif
+#endif  // PGL_FREE
+
+#ifndef PGL_MEMMOVE
+#define PGL_MEMMOVE(dest, src, n) PNTR_MEMCPY((dest), (src), (n))
+#endif  // PGL_MEMMOVE
+
+// Prefix all types with 'pgl_'
+#define PGL_PREFIX_TYPES
 
 #ifndef PNTR_PORTABLEGL_H
 #define PNTR_PORTABLEGL_H "./vendor/portablegl.h"
 #endif
+
 #include PNTR_PORTABLEGL_H
 
-glContext* pntr_init_glContext(pntr_image* screen);
-void basic_vs(float* vs_output, vec4* vertex_attribs, Shader_Builtins* builtins, void* uniforms);
-void basic_fs(float* fs_input, Shader_Builtins* builtins, void* uniforms);
+/**
+ * Initialize the OpenGL context for the given destination image.
+ *
+ * @param dst The destination image to initialize the OpenGL context for.
+ *
+ * @return The initialized OpenGL context, or NULL on failure.
+ */
+glContext* pntr_init_glContext(pntr_image* dst);
 
 #endif  // PNTR_PORTABLEGL_H__
 
@@ -29,12 +44,12 @@ void basic_fs(float* fs_input, Shader_Builtins* builtins, void* uniforms);
 #define PORTABLEGL_IMPLEMENTATION
 #include PNTR_PORTABLEGL_H
 
-glContext* pntr_init_glContext(pntr_image* screen) {
-    if (screen == NULL) {
+glContext* pntr_init_glContext(pntr_image* dst) {
+    if (dst == NULL) {
         return NULL;
     }
 
-    if (screen->subimage) {
+    if (dst->subimage) {
         return NULL;
     }
 
@@ -44,10 +59,11 @@ glContext* pntr_init_glContext(pntr_image* screen) {
     }
 
     #ifdef PNTR_PIXELFORMAT_RGBA
-        u32 Rmask = 0xFF000000;
-        u32 Gmask = 0x00FF0000;
-        u32 Bmask = 0x0000FF00;
-        u32 Amask = 0x000000FF;
+        // TODO: PortableGL - Fix the RGBA mask?
+        u32 Rmask = 0x000000FF;
+        u32 Gmask = 0x0000FF00;
+        u32 Bmask = 0x00FF0000;
+        u32 Amask = 0xFF000000;
     #elif defined(PNTR_PIXELFORMAT_ARGB)
         #if defined(SDL_BYTEORDER) && SDL_BYTEORDER == SDL_BIG_ENDIAN
             u32 Rmask = 0xFF000000;
@@ -62,22 +78,13 @@ glContext* pntr_init_glContext(pntr_image* screen) {
         #endif
     #endif
 
-    u32* backbuffer = (u32*)screen->data;
-    if (!init_glContext(context, &backbuffer, screen->width, screen->height, sizeof(uint32_t), Rmask, Gmask, Bmask, Amask)) {
+    u32* backbuffer = (u32*)dst->data;
+    if (!init_glContext(context, &backbuffer, dst->width, dst->height, 32, Rmask, Gmask, Bmask, Amask)) {
         PNTR_FREE(context);
         return NULL;
     }
 
     return context;
-}
-
-void basic_vs(float* vs_output, vec4* vertex_attribs, Shader_Builtins* builtins, void* uniforms)
-{
-	builtins->gl_Position = vertex_attribs[0];
-}
-void basic_fs(float* fs_input, Shader_Builtins* builtins, void* uniforms)
-{
-	builtins->gl_FragColor = make_vec4(1.0f, 0.5f, 0.2f, 1.0f);
 }
 
 #endif  // PNTR_PORTABLEGL_IMPLEMENTATION_ONCE
