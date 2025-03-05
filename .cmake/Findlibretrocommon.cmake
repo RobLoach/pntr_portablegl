fc063039c6a0f2fd63df32d41b50e9fe1c06faf2
Rob Loach <robloach@gmail.com>
2025-03-04 23:30:49 -0500

    Update

--- .cmake/Findlibretrocommon.cmake
+++ .cmake/Findlibretrocommon.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     libretrocommon
     GIT_REPOSITORY https://github.com/libretro/libretro-common.git
-    GIT_TAG c273a49
+    GIT_TAG ad9124f
 )
 FetchContent_MakeAvailable(libretrocommon)
 #include_directories(${libretrocommon_SOURCE_DIR})
